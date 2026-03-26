07767e7ed45b34f9553764e3922058c7b57287c4
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2026-03-26 17:26:48 +0100

    fix: adds logout button on the sidebar

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { authClient } from '@/lib/auth-client';
-import { LogOut } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { Links } from '@/lib/enums/links';
+import { authClient } from "@/lib/auth-client";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Links } from "@/lib/enums/links";
 
 export const handleLogout = async (router: ReturnType<typeof useRouter>) => {
   await authClient.signOut();
@@ -15,9 +15,10 @@ export function Logout({ classes }: { classes?: string } = {}) {
 
   return (
     <span
-      className={`w-full cursor-pointer ${classes ?? ''}`}
-      onClick={() => handleLogout(router)}>
-      Logout <LogOut className="size-4" />
+      className={`flex h-8 w-full cursor-pointer items-center rounded-md text-sm ${classes ?? ""}`}
+      onClick={() => handleLogout(router)}
+    >
+      <LogOut className="size-4" /> Logout
     </span>
   );
 }
