479ee526e4e1b6a34906b7505d1be2f108c6122b
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2026-04-19 21:16:40 +0100

    fix: cross origin conflict

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,6 @@
-export const BACKEND_URL =
-  process.env.BETTER_AUTH_API_URL || 'http://localhost:8000';
-export const FRONTEND_URL =
-  process.env.BETTER_AUTH_URL || 'http://localhost:3000';
-export const SECRET_KEY =
-  process.env.BETTER_AUTH_SECRET || 'better_auth_secret_key';
+export const BACKEND_URL = process.env.BETTER_AUTH_API_URL!;
+export const FRONTEND_URL = process.env.BETTER_AUTH_URL!;
+export const SECRET_KEY = process.env.BETTER_AUTH_SECRET || "better_auth_secret_key";
 export const DATABASE_URL = process.env.DATABASE_URL!;
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
