139c78adaafef3d4cd9577998297feaa390a440d
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2026-04-19 18:28:30 +0100

    chore: removing the auth provider

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,26 +1,30 @@
-import SidebarWrapper from "@/components/dashboard/sidebar-wrapper";
+import AppSidebar from "@/components/dashboard/sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AuthProvider } from "@/providers/AuthProvider";
+import { unauthorized } from "next/navigation";
+import { getAuthSession } from "../actions/auth";
 
 export default async function ProtectedRouteLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getAuthSession();
+  if (!session) {
+    return unauthorized();
+  }
+
   return (
-    <AuthProvider>
-      <div className="flex bg-[#F7F4F5]">
-        <SidebarProvider>
-          <div className="hidden lg:block">
-            <SidebarWrapper />
-          </div>
-          {/* Main Content Area */}
-          <main className="flex-1 overflow-y-auto p-4">
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
-      </div>
-    </AuthProvider>
+    <div className="flex bg-[#F7F4F5]">
+      <SidebarProvider>
+        <div className="hidden lg:block">
+          <AppSidebar user={session.user} />
+        </div>
+        {/* Main Content Area */}
+        <main className="flex-1 overflow-y-auto p-4">
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </div>
   );
 }
